5ba18965ab1f2241b8556cc0a1f6a83b01febbf8
boerdie92 <jonathan.doering92@gmail.com>
2025-02-14 17:26:48 +0100

    fck me, its gettin hard

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 timer = pygame.time.Clock()
+
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 dt = 0
 
@@ -16,6 +19,7 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = timer.tick(60) / 1000
 
