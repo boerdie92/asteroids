f27801e64d8769beaa9b528f6871b3aa515c03a4
boerdie92 <jonathan.doering92@gmail.com>
2025-02-18 21:55:05 +0100

    asteroidfields fckn wrkn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def init_game():
     pygame.init()
@@ -9,16 +11,20 @@ def init_game():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
     Player.containers = (updatable, drawable)
-    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
-    return screen, timer, updatable, drawable
+    asteroidfield = AsteroidField()
+
+    return screen, timer, updatable, drawable, asteroids, asteroidfield
 
 def main():
-    screen, timer, updatable, drawable = init_game()
-    
+    screen, timer, updatable, drawable, asteroids, asteroidfield = init_game()
+
     while True:
         dt = timer.tick(60) / 1000
 
